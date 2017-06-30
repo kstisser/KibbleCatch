0cddc26ec7a40732bb5cae44f3cf20520a8f8cdd
kstisser <fm8733@gmail.com>
2017-06-30 13:54:02 -0400

    Initial project commit

---
+++ Assets/Scripts/Installers/MainInstaller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace KibbleSpace
+{
+    //Installer for Scene Context
+    public class MainInstaller : MonoInstaller<MainInstaller>
+    {
+        public GameObject Dog;
+        public GameObject KibblePrefab;
+
+        public override void InstallBindings()
+        {
+            
+        }
+
+    }
+}
